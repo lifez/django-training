007ed893f7885bdc918408b443c34a156be8b9d2
Phawin Khongkhasawan <lifez999@gmail.com>
2019-08-20 14:24:47 +0700

    Add CreateView

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -55,7 +55,6 @@ ROOT_URLCONF = 'instagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- instagram/posts/views.py
+++ instagram/posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .models import Post
+
+class CreatePostView(CreateView):
+    model = Post
+    template_name = 'post_form.html'
+    fields = ['text']
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
