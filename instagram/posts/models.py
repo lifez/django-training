14666cf3a092f2896050a895e0831376ff277b3d
Phawin Khongkhasawan <lifez999@gmail.com>
2019-08-20 14:04:01 +0700

    Add Post model

--- instagram/posts/models.py
+++ instagram/posts/models.py
@@ -1,2 +1,6 @@
 from django.db import models
 
+
+class Post(models.Model):
+    text = models.CharField(max_length=256, null=False, blank=False)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
